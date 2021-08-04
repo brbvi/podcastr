133bd570a1ab72fd5f35975566873797f9af6aa5
brbvi <vito.borba@gmail.com>
2021-08-04 07:14:16 -0300

    feat: added new font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,6 +38,14 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lexend:wght@500;600&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
